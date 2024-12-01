5778c3f323ef4a9f42fc48602a1bb1a061d0502d
MutableName96 <antoniolopezmartinez45@gmail.com>
2024-12-01 00:08:43 -0600

    paginacion update

--- Code/Paginacion.c
+++ Code/Paginacion.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct TablePage{
+	int indice;
+	int contenido;
+	int cargada;
+	
+}TablePage;
+
+int NumRandom(){
+	return (rand() %5);
+	
+	}
 
 
 
 int main()
 {
+	srand(time(NULL));
+	printf("%d",NumRandom());
+
 	
 
     
   return 0 ;
-  }
+ }
