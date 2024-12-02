ea2856279a07d4d022035514fc907a0b37fafa9a
MutableName96 <antoniolopezmartinez45@gmail.com>
2024-12-02 08:31:56 -0600

    uptd

--- Code/manejadorDispositivosSencillo.c
+++ Code/manejadorDispositivosSencillo.c
@@ -1,26 +1,49 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+
 
 bool printerStatus = false; //f->close t->open
 char printerData[256]; // buffer
 
 void open(){
-	
+	if(!printerStatus){
+		printerStatus = true;
+	} else{
+		printf("La impresora esta abierta");
+		
+		}
 }
 
-void read(){
+void readBuffer(){
+	if(printerStatus){
+		printf("leyendo...");
+		sleep(0.5);	
+		printf("%s",printerData);
+	} else{
+		printf("La impresora esta cerrada");
+		
+		}
 }
 
-void write(){
+void writeBuffer(){
 }
 	
-void close(){
+void closePrintf(){
+	if(printerStatus){
+		printerStatus=false;
+	} else{
+		printf("Ya esta cerrada");
+		
+		}
+		
 }
 
 int main(){
 	
 	fgets(printerData,sizeof(printerData),stdin);
 	printf("%s",printerData);
+	read();
 	return 0;
 	}
