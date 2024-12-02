4bfed2df31506f99894ee578028f177aae52398c
MutableName96 <antoniolopezmartinez45@gmail.com>
2024-12-02 15:33:12 -0600

    4.3 finish

--- Code/manejadorDispositivosTablaDesEstructura.c
+++ Code/manejadorDispositivosTablaDesEstructura.c
@@ -1,78 +1,96 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
 /****************************************
- *         		Struct's                *
+ *              Struct's                *
  ****************************************/
-typedef struct Device{
-	bool interrupcion;
-	}Device;
+typedef struct Device {
+    bool interrupcion;
+    int id;
+} Device;
 
-typedef struct TablaEstadosDispositivos{
-	int estado; // 0 -> espera 1-> ocupado/p en proceso 2-> completado
-	int parameters; //parametros especiales
-	}TablaEstadosDispositivos;
+typedef struct TablaEstadosDispositivos {
+    int estado; // 0 -> espera, 1 -> en proceso, 2 -> completado
+    int parameters; // parámetros especiales
+} TablaEstadosDispositivos;
 
 /****************************************
  *        Variables Globales            *
  ****************************************/
-Device maus;
-TablaEstadosDispositivos tablaEstados;
-bool interrupcionActiva = false;
+#define Devices 5 
+Device dispositivos[Devices];
+TablaEstadosDispositivos tablaEstados[Devices];
+bool interrupcionActiva[Devices] = { false };
+
+
 
 /****************************************
- *         		Funciones               *
+ *              Funciones               *
  ****************************************/
- void ES();
- void atenderES();
- 
- void driverDevice(Device dispositivo){
-	 while (tablaEstados.estado==1){
-		 printf("Dispositivo ocupado\n");
-		 sleep(1);
-		 printf("Espere...\n");
-		 sleep(2);
-		 tablaEstados.estado=0;
-		 }
-		 
-	if(tablaEstados.estado==0){
-		tablaEstados.estado=1;
-		printf("info: device libre\n");
-		ES();
-		atenderES();
-	
-	}	 
-	 
+void ES(int id);
+void atenderES(int id);
+void driverDevice(Device* dispositivo);
+
+void driverDevice(Device* dispositivo) {
+    int id = dispositivo->id;
+
+    while (tablaEstados[id].estado == 1) { 
+        printf("Dispositivo %d ocupado\n", id);
+        sleep(1);
+        printf("Espere...\n");
+        sleep(3);
+        tablaEstados[id].estado=0;
+        
+    }
+
+    if (tablaEstados[id].estado == 0) { 
+        tablaEstados[id].estado = 1;
+        printf("Info: device %d libre\n", id);
+        ES(id);
+        atenderES(id);
+    }
 }
 
-void ES(){
-	printf("Iniciando operación de E/S...\n");
+void ES(int id) {
+    printf("Iniciando operación de E/S en dispositivo %d...\n", id);
     sleep(2);
-    tablaEstados.estado = 2;
-    printf("Operación de E/S cargada en la tabla de estados.\n");
+    tablaEstados[id].estado = 2;
+    interrupcionActiva[id] = true;
+    printf("Operación de E/S en dispositivo %d cargada en la tabla de estados.\n", id);
 }
 
-void atenderES(){
-	if(tablaEstados.estado == 2){
-		printf("interrupcion\n");
-		printf("atendiendo interrupcion\n");
-		tablaEstados.estado = 0;
-		}
-	
+void atenderES(int id) {
+    if (interrupcionActiva[id]) {
+        printf("Interrupción en dispositivo %d\n", id);
+        printf("Atendiendo interrupción en dispositivo %d\n", id);
+        tablaEstados[id].estado = 0;
+        interrupcionActiva[id] = false;
+    }
 }
 
 
-int main(){
+int main() {
 	
-	maus.interrupcion = false;
-    tablaEstados.estado = 1; 
-    tablaEstados.parameters = 0;
-    while(true){
-		printf("\n");
-		driverDevice(maus);
-		sleep(3);
-		}
+	srand(time(NULL));
 	
-	return 0;
+    for (int i = 0; i < Devices; i++) {
+        dispositivos[i].interrupcion = false;
+        dispositivos[i].id = i;
+        tablaEstados[i].estado = rand()%2;
+        tablaEstados[i].parameters = rand()%10;
+    }
+
+    while (true) {
+        for (int i = 0; i < Devices; i++) {
+			printf("\n");
+            driverDevice(&dispositivos[i]);
+            sleep(3);
+        }
+    }
+
+    return 0;
 }
+
--- main.md
+++ main.md
@@ -1810,8 +1810,104 @@ int main() {
 ```
 ---
 
-**2.- Escribe un programa que simule las operaciones de un manejador dedispositivos utilizando una tabla de estructuras.**
+**2.- Escribe un programa que simule las operaciones de un manejador de dispositivos utilizando una tabla de estructuras.**
 ```C
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+/****************************************
+ *              Struct's                *
+ ****************************************/
+typedef struct Device {
+    bool interrupcion;
+    int id;
+} Device;
+
+typedef struct TablaEstadosDispositivos {
+    int estado; // 0 -> espera, 1 -> en proceso, 2 -> completado
+    int parameters; // parámetros especiales
+} TablaEstadosDispositivos;
+
+/****************************************
+ *        Variables Globales            *
+ ****************************************/
+#define Devices 5 
+Device dispositivos[Devices];
+TablaEstadosDispositivos tablaEstados[Devices];
+bool interrupcionActiva[Devices] = { false };
+
+
+
+/****************************************
+ *              Funciones               *
+ ****************************************/
+void ES(int id);
+void atenderES(int id);
+void driverDevice(Device* dispositivo);
+
+void driverDevice(Device* dispositivo) {
+    int id = dispositivo->id;
+
+    while (tablaEstados[id].estado == 1) { 
+        printf("Dispositivo %d ocupado\n", id);
+        sleep(1);
+        printf("Espere...\n");
+        sleep(3);
+        tablaEstados[id].estado=0;
+        
+    }
+
+    if (tablaEstados[id].estado == 0) { 
+        tablaEstados[id].estado = 1;
+        printf("Info: device %d libre\n", id);
+        ES(id);
+        atenderES(id);
+    }
+}
+
+void ES(int id) {
+    printf("Iniciando operación de E/S en dispositivo %d...\n", id);
+    sleep(2);
+    tablaEstados[id].estado = 2;
+    interrupcionActiva[id] = true;
+    printf("Operación de E/S en dispositivo %d cargada en la tabla de estados.\n", id);
+}
+
+void atenderES(int id) {
+    if (interrupcionActiva[id]) {
+        printf("Interrupción en dispositivo %d\n", id);
+        printf("Atendiendo interrupción en dispositivo %d\n", id);
+        tablaEstados[id].estado = 0;
+        interrupcionActiva[id] = false;
+    }
+}
+
+
+int main() {
+	
+	srand(time(NULL));
+	
+    for (int i = 0; i < Devices; i++) {
+        dispositivos[i].interrupcion = false;
+        dispositivos[i].id = i;
+        tablaEstados[i].estado = rand()%2;
+        tablaEstados[i].parameters = rand()%10;
+    }
+
+    while (true) {
+        for (int i = 0; i < Devices; i++) {
+			printf("\n");
+            driverDevice(&dispositivos[i]);
+            sleep(3);
+        }
+    }
+
+    return 0;
+}
+
 
 ```
 
