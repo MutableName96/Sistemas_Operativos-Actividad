9884287c1a7d71ef02e05011e0099005a2c1f2f5
MutableName96 <antoniolopezmartinez45@gmail.com>
2024-12-02 08:06:24 -0600

    update

--- Code/manejadorDispositivosSencillo.c
+++ Code/manejadorDispositivosSencillo.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool printerStatus = false; //f->close t->open
+char printerData[256]; // buffer
+
+void open(){
+	
+}
+
+void read(){
+}
+
+void write(){
+}
+	
+void close(){
+}
+
 int main(){
+	
+	fgets(printerData,sizeof(printerData),stdin);
+	printf("%s",printerData);
 	return 0;
 	}
