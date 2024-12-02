6f216cb05d59a0f99bbccacd81792b2c0ac9b867
MutableName96 <antoniolopezmartinez45@gmail.com>
2024-12-02 13:28:42 -0600

    idea

--- Code/ManejoInterrupcionesBasic.c
+++ Code/ManejoInterrupcionesBasic.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+typedef struct Device{
+	bool andochingando;
+	}Device;
+
+typedef struct TablaEstados{
+	//direccionRetorno;
+	int parameters;
+	}TablaEstados;
+int main(){
+	
+	return 0;
+}
