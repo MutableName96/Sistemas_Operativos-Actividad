659881928078448d6a15d0d394b5ec80c70f9ed3
MutableName96 <antoniolopezmartinez45@gmail.com>
2024-12-02 19:59:56 -0600

    I got it from my daddy

--- Code/discoMagnetico.c
+++ Code/discoMagnetico.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 /****************************************
  *              Struct's                *
  ****************************************/
- 
+ typedef struct CabezaL{
+	 bool busy;
+	 
+	 }CabezaL;
+	 
+typedef struct Archivo{
+	int size;
+	
+	}Archivo;
  /****************************************
  *        Variables Globales            *
  ****************************************/
+ CabezaL cabeza = {false};
  
  /****************************************
  *              Funciones               *
